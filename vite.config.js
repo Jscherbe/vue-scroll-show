f5be6d65b464f388128f633912c409879e6bc255
Joe Scherben <joe.scherben@informatics-studio.com>
2024-03-12 14:58:49 -0400

    Change docs base url (demo)

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   ],
   build: {
     lib: {
-      entry: resolve(__dirname, "lib/index.js"),
+      entry: resolve(__dirname, "lib/index.ts"),
       name: "VueScrollShow",
       fileName: "lib",
     },
